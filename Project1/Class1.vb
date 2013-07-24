0ca3bc832406a111f383c85b43af8b7d20040783
edualves.bsb <edualves.bsb@gmail.com>
2013-07-24 17:25:05 -0300

    Multiplicação 2 X 5

--- Project1/Class1.vb
+++ Project1/Class1.vb
@@ -6,5 +6,14 @@ Class OlaMundo
         WriteLine("Olá, Mundo!")
         WriteLine("Test GitHub OK!!")
         WriteLine("Test Visual Studio OK!!")
+
+        Dim Valor1 As Integer = 5
+        Dim Valor2 As Integer = 2
+        Dim Resultado As Integer
+
+        Resultado = Valor1 * Valor2
+
+        Console.WriteLine(Resultado.ToString)
+
     End Sub
 End Class
